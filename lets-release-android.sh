911db2a2c4ae9dc1a7cb23345f8d9215092734fa
Mayel de Borniol <mayel@deborniol.com>
2017-11-30 17:50:11 +0200

    new android build

--- lets-release-android.sh
+++ lets-release-android.sh
@@ -4,8 +4,8 @@ echo "WARNING! Did you remember to change the version and versionCode numbers in
 
 ionic cordova build --release android # compile
 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore lets-android.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk M_LETS # sign
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore lets-android.keystore platforms/android/build/outputs/apk/release/android-release-unsigned.apk M_LETS # sign
 
 rm LETS_app_beta.apk # delete old version
 
-~/Library/Android/sdk/build-tools/27.0.1/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk LETS_app_beta.apk # prepare apk
+~/Library/Android/sdk/build-tools/27.0.1/zipalign -v 4 platforms/android/build/outputs/apk/release/android-release-unsigned.apk LETS_app_beta.apk # prepare apk
